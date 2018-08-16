76a788941a582b07ac9bae0417ad4ef4fe42cbe5
itsmesravs <41770057+itsmesravs@users.noreply.github.com>
2018-08-16 14:35:18 +0530

    Create amstrong.py

---
+++ amstrong.py
@@ -0,0 +1,11 @@
+a=int(raw_input())
+b=int(raw_input())
+for i in range(a,b):
+   sum=0
+   temp=n
+   while temp > 0:
+       digit = temp % 10
+       sum =sum+ digit ** 3
+       temp /= 10
+if n == sum:
+       print(n)
